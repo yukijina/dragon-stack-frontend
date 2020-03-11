d6e2df7171048292d31a127bf23587422779b718
yukijina <yukijina@gmail.com>
2020-03-11 11:15:50 -0700

    sec.65 add state and componentDidMount

--- src/components/Generation.js
+++ src/components/Generation.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class Generation extends Component {
+  state = {
+    generation: { generationId: 999, expiration: '2020-05-01'}
+  };
+
+  componentDidMount() {
+    this.fetchGeneration
+  }
+
+  fetchGeneration = () => {
+    fetch('http://localhost:3000/generation')
+    .then(response => console.log('response', response));
+  }
+
+
   render() {
-    const generation = { generationId: 999, expiration: '2020-05-01'}
-    
+    const {generation} = this.state
+
     return (
       <div>
         <h3>Generation {generation.generationId}. Expires on:</h3>
