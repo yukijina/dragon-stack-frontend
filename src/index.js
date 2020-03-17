209bb1dadd82aa9684795284e5d35b2f7c9933cc
yukijina <yukijina@gmail.com>
2020-03-16 18:19:06 -0700

    sec.80 add reducer and create store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { createStore } from 'redux';
 import { render } from 'react-dom';
 import Generation from './components/Generation';
 import Dragon from './components/Dragon';
 
 import './index.css';
+
+const DEFAULT_GENERATION = { generationId: '', expiration: '' };
+const generationReducer = () => {
+  return {
+    generation: DEFAULT_GENERATION
+  }
+}
+const store = createStore(generationReducer);
+
+console.log('this is store: ', store)
 
 render(
   <div>
