6377c22b587ce2e464cdff40487d71f1a5d210ab
yukijina <yukijina@gmail.com>
2020-03-16 18:34:36 -0700

    sec.81 crate action

--- src/index.js
+++ src/index.js
@@ -7,14 +7,24 @@ import Dragon from './components/Dragon';
 import './index.css';
 
 const DEFAULT_GENERATION = { generationId: '', expiration: '' };
-const generationReducer = () => {
+const generationReducer = (state, action) => {
+
+  if (action.type === 'GENERATION_ACTION_TYPE') {
+    return { generation: action.generation };
+  }
   return {
     generation: DEFAULT_GENERATION
   }
 }
 const store = createStore(generationReducer);
 
-console.log('this is store: ', store)
+store.dispatch({ type: 'foo' });
+store.dispatch({
+  type: 'GENERATION_ACTION_TYPE',
+  generation: { generationId: 'goo', expiration: 'bar' }
+})
+
+
 
 render(
   <div>
