bbdae572e70fa2581db6c5cd34d86cea8ccc6854
yukijina <yukijina@gmail.com>
2020-03-18 18:46:58 -0700

    sec.85 crate mapDispatchToProps

--- src/components/Generation.js
+++ src/components/Generation.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { generationActionCreator } from '../actions/generation';
 
 const DEFAULT_GENERATION = { generationId: '', expiration: ''}
 
 const MININUM_DELAY = 3000;
 
 class Generation extends Component {
-  state = {
-    generation: DEFAULT_GENERATION
-  };
+  // state = {
+  //   generation: DEFAULT_GENERATION
+  // };
 
   timer = null;
 
@@ -21,20 +22,19 @@ class Generation extends Component {
   }
 
   fetchGeneration = () => {
-    console.log("hitting")
     fetch('http://localhost:3000/generation')
     .then(response => response.json())
     .then(json => { 
-      console.log('json:', json)
-      this.setState({ generation: json.generation })
+      // this.setState({ generation: json.generation })
+      this.props.dispatchGeneration(json.generation)
     })
-    .catch(err => console.log('error:', error));
+    .catch(err => console.log('error:', err));
   }
 
   fetchNextGeneration = () => {
     this.fetchGeneration();
 
-    let delay = new Date(this.state.generation.expiration).getTime() - new Date().getTime();
+    let delay = new Date(this.props.generation.expiration).getTime() - new Date().getTime();
 
     if (delay < MININUM_DELAY) {
       delay = MININUM_DELAY
@@ -57,12 +57,21 @@ class Generation extends Component {
   }
 }
 
+
 const mapStateToProps = state => {
   const generation = state.generation;
-
   return { generation }
 }
 
-const componentConnector = connect(mapStateToProps);
+  
+const mapDispatchToProps = dispatch => {
+  return {
+    dispatchGeneration: generation => dispatch(
+      generationActionCreator(generation)
+    )
+  }  
+}
+
+const componentConnector = connect(mapStateToProps, mapDispatchToProps);
 
 export default componentConnector(Generation);
