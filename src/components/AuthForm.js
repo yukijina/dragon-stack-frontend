7e27de29bb7473b86bdb0bec0fdd163a27783a3c
yukijina <yukijina@gmail.com>
2020-04-07 19:37:36 -0700

    sec.116 organize files

---
+++ src/components/AuthForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AuthForm extends Component {
+  render() {
+    return (
+      <div>Auth Form</div>
+    )
+  }
+}
+
+export default AuthForm;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import thunk from 'redux-thunk';
-import Generation from './components/Generation';
-import Dragon from './components/Dragon';
 import rootReducer from './reducers';
+import Root from './components/Root';
 // import { generationActionCreator } from './actions/generation'
 import './index.css';
 
@@ -36,11 +35,7 @@ const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 render(
   <Provider store={store}>
-    <div>
-      <h2>Dragon Stack</h2>
-      <Generation />
-      <Dragon />
-    </div>
+    <Root />
   </Provider>,
   document.getElementById('root')
 );
