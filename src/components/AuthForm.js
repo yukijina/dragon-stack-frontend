350ce56578dda34e3236cafbfd9c3433d8eaadcd
yukijina <yukijina@gmail.com>
2020-04-08 20:40:48 -0700

    sec.118 add onChange and onClick for the form

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -4,7 +4,25 @@ import { Button, FormGroup, FormControl } from 'react-bootstrap';
 class AuthForm extends Component {
   state = { username: '', password: '' };
 
+  updateUsername = event => {
+    this.setState({ username: event.target.value })
+  }
+
+  updatePassword = event => {
+    this.setState({ password: event.target.value })
+  }
+
+  signup = () => {
+    console.log('signup this.state:', this.state)
+  }
+
+  login = () => {
+    console.log('login this.state: ', this.state)
+  }
+
   render() {
+
+
     return (
       <div>
         <h2>Dragon Stack</h2>
@@ -13,6 +31,7 @@ class AuthForm extends Component {
           type='text'
           value={this.state.username}
           placeholder='username'
+          onChange={this.updateUsername}
           />
         </FormGroup>
         <FormGroup>
@@ -20,12 +39,13 @@ class AuthForm extends Component {
           type='password'
           value={this.state.password}
           placeholder='password'
+          onChange={this.updatePassword}
           />
         </FormGroup>
         <div>
-          <Button>Log In</Button>
+          <Button onClick={this.login}>Log In</Button>
           <span> or </span>
-          <Button>Sign Up</Button>
+          <Button onClick={this.signup}>Sign Up</Button>
         </div>
       </div>
     )
