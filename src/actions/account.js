b606013eeb3a4dbf8ae5191ea6f73588a17b3ead
yukijina <yukijina@gmail.com>
2020-04-19 20:32:30 -0700

    sec.124 create helper for signup and logout function. Clean the codes

--- src/actions/account.js
+++ src/actions/account.js
@@ -1,40 +1,37 @@
 import { ACCOUNT } from './types';
 import { BACKEND } from '../config';
 
-export const signup = ({ username, password }) => dispatch => {
+const fetchFromAccount = ({ endpoint, options, SUCCESS_TYPE }) => dispatch => {
   dispatch({ type: ACCOUNT.FETCH });
 
-  return fetch(`${BACKEND.ADDRESS}/account/signup`, {
+  return fetch(`${BACKEND.ADDRESS}/account/${endpoint}`, options)
+  .then(response => response.json())
+  .then(json => {
+    if(json.type === 'error') {
+      dispatch({ type: ACCOUNT.FETCH_ERROR, message: json.message })
+    } else {
+      dispatch({ type: SUCCESS_TYPE, ...json})
+    }
+  }).catch(error => dispatch({
+    type: ACCOUNT.FETCH_ERROR, message: error.message
+  }))
+};
+
+
+export const signup = ({ username, password }) => fetchFromAccount({
+  endpoint: 'signup',
+  options: {
     method: 'POST',
     body: JSON.stringify({ username, password }),
     headers: { 'Content-Type': 'application/json'},
     //store the cookie sessions to browser
     credentials: 'include'
-  }).then(response => response.json())
-  .then(json => {
-    if(json.type === 'error') {
-      dispatch({ type: ACCOUNT.FETCH_ERROR, message: json.message })
-    } else {
-      dispatch({ type: ACCOUNT.FETCH_SUCCESS, ...json})
-    }
-  }).catch(error => dispatch({
-    type: ACCOUNT.FETCH_ERROR, message: error.message
-  }))
-}
+  },
+  SUCCESS_TYPE: ACCOUNT.FETCH_SUCCESS
+});
 
-export const logout = () => dispatch => {
-  dispatch({ type: ACCOUNT.FETCH });
-
-  return fetch(`${BACKEND.ADDRESS}/account/logout`, {
-    credentials: 'include'
-  }).then(response => response.json())
-  .then(json => {
-    if (json.type === 'error') {
-      dispatch({ type: ACCOUNT.FETCH_ERROR, message: json.message })
-    } else {
-      dispatch({ type: ACCOUNT.FETCH_LOGOUT_SUCCESS, ...json});
-    }
-  })
-  .catch(error => dispatch({ type: ACCOUNT.FETCH_ERROR, message: error.message }))
-}
-
+export const logout = () => fetchFromAccount({
+  endpoint: 'logout',
+  options: { credentials: 'include' },
+  SUCCESS_TYPE: ACCOUNT.FETCH_LOGOUT_SUCCESS
+});
