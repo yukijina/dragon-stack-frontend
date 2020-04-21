7f07f2d738df12b15fb420f88a24f33a3dd85208
yukijina <yukijina@gmail.com>
2020-04-20 19:25:27 -0700

    update logout function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
 import Generation from './Generation';
 import Dragon from './Dragon';
+import { logout } from '../actions/account';
 
 
 class Home extends Component {
   render() {
     return(
       <div>
+        <Button onClick={this.props.logout} className='logout-button'>Log Out</Button>
       <h2>Dragon Stack</h2>
       <Generation />
       <Dragon />
@@ -16,4 +20,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default connect(null, { logout })(Home);
