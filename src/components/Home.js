79d5dc28dc04d935c5cd73f56176c4638350d56f
yukijina <yukijina@gmail.com>
2020-05-08 22:11:27 -0700

    sec.140 add debugging code for fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,4 +20,9 @@ class Home extends Component {
   }
 }
 
+fetch('http://localhost:3000/account/dragons', {
+  credentials: 'include'
+}).then(response => response.json())
+.then(json => console.log('account dragons', json))
+
 export default connect(null, { logout })(Home);
