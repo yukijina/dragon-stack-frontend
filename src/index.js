60228f89f65f7d119127df532a1eb401746f8872
yukijina <yukijina@gmail.com>
2020-05-20 20:10:37 -0700

    sec.148 add redirect from react-router'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { Router, Switch, Route } from'react-router-dom';
+import { Router, Switch, Route, Redirect } from'react-router-dom';
 import { render } from 'react-dom';
 import thunk from 'redux-thunk';
 import { createBrowserHistory } from 'history';
@@ -17,6 +17,14 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
+class RedirectToAccountDragons extends Component {
+  render() {
+    return (
+      <Redirect to={{ pathname: '/account-dragons '}} />
+    )
+  }
+}
+
 store.dispatch(fetchAuthenticated())
 .then(() => {
   render(
@@ -25,6 +33,7 @@ store.dispatch(fetchAuthenticated())
         <Switch>
           <Route exact path='/' component={Root} />
           <Route path='/account-dragons' component={AccountDragons} />
+          <Route path='/redirect-to-account-dragons' component={RedirectToAccountDragons} />
         </Switch>
       </Router>  
     </Provider>,
