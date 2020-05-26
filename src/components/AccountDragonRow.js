cee619a01e43ecc5d22f91547c3a4bb9c2db1227
yukijina <yukijina@gmail.com>
2020-05-25 23:27:25 -0700

    sec.152 add edit button and updateNickname function

--- src/components/AccountDragonRow.js
+++ src/components/AccountDragonRow.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import DragoNAvatar from './DragonAvatar';
+import { Button } from 'react-bootstrap';
+import DragonAvatar from './DragonAvatar';
 
 class AccountDragonRow extends Component {
+  state = {
+    nickname: this.props.dragon.nickname,
+    edit: false
+  };
+
+  updateNickname = event => {
+    this.setState({
+      nickname: event.target.value
+    })
+  }
+
+  toggleEdit = () => {
+    this.setState({ edit: !this.state.edit})
+  }
+
+  get SaveButton() {
+    return <Button>Save</Button>
+  }
+
+  get EditButton() {
+    return <Button onClick={this.toggleEdit}>Edit</Button>
+  }
+
   render() {
     return (
       <div>
         <div>{this.props.dragon.nickname}</div>
+        <input type="text" value={this,state.nickname} onChange={this.updateNickname} disabled={!this.state.edit} />
         <br />
         <DragonAvatar dragon={this.props.dragon} />
+        { 
+          this.state.edit ? this.SaveButton : this.EditButton
+        }
       </div>
     )
   }
